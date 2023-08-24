0d37dd368eec6ee7172533bfc294a67c3babdfbd
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-24 11:10:58 +0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from "./Component/Home"
+import UserListing from './Component/UserListing'
+import Adduser from './Component/Adduser'
+import Updateuser from './Component/Updateuser'
 
 function App() {
   return (
     <div className="App">
-  App
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}  />
+    <Route path='/user' element={<UserListing/>}  />
+    <Route path='/user/add' element={<Adduser/>}  />
+    <Route path='/user/edit/:code' element={<Updateuser/>}  />
+
+  </Routes>
+  </BrowserRouter>
     </div>
   );
 }
